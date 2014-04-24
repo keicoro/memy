11755c0922dd27f3f3ca7de61bbce520d332b292
Kei Ikegami <kei@funkycorp.com>
2014-04-24 14:28:12 +0300

    always show debug toolbar with local setting

--- memy_project/memy_project/memy/settings/local.py
+++ memy_project/memy_project/memy/settings/local.py
@@ -60,5 +60,10 @@ MIDDLEWARE_CLASSES += (
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-INTERNAL_IPS = ('127.0.0.1',)
+def always_show_toolbar(self):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': always_show_toolbar,
+}
 ########## END TOOLBAR CONFIGURATION
