8725b32c8fa45602811f4e1b2849466f48879929
Kei Ikegami <kei@funkycorp.com>
2014-04-26 00:52:35 +0300

    - edit nginx log format - add gunicorn to installed apps

--- memy_project/memy_project/memy/urls.py
+++ memy_project/memy_project/memy/urls.py
@@ -26,6 +26,9 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     import debug_toolbar
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
     urlpatterns += patterns('',
                             url(r'^__debug__/', include(debug_toolbar.urls)),
                             )
+    urlpatterns += staticfiles_urlpatterns()
