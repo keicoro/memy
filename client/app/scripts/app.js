832bd8fd42bc10ff9592d5733aae7088ec09507b
Kei Ikegami <kei@funkycorp.com>
2014-06-08 22:29:05 +0300

    import ionic swipecards into project

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -6,7 +6,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('Client', ['ionic', 'Client.controllers'])
+angular.module('Client', ['ionic', 'ngTouch', 'ionic.contrib.ui.cards', 'Client.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
